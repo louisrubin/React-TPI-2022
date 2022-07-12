5fdf0e0a7e95c769058931a71d378b3ba94fb885
louisrubin <miguel10.rbn@gmail.com>
2022-07-12 18:34:17 -0300

    trying DateTime with luxon failed --> News.js

--- react_tpi_2022/src/components/News.js
+++ react_tpi_2022/src/components/News.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DateTime } from 'luxon'
 import './News-css.css'
 
 
@@ -6,10 +7,11 @@ import './News-css.css'
 
 function Main() {
     const search = 'messi'
-    const url = `https://newsapi.org/v2/everything?q=${search}&pageSize=5&apiKey=3a8f8a50766947e8b6d4633919806d8a`
+    const pages = 7
+    
+    const url = `https://newsapi.org/v2/everything?q=${search}&pageSize=${pages}&apiKey=3a8f8a50766947e8b6d4633919806d8a`
     console.log(process.env.NEWS_API_KEY)
     const [toDos, setToDos] = useState()
-    const [imgs, setImage] = useState()
     const [totalResult, setResult] = useState()
 
     const fetchApi = async () => {
@@ -25,17 +27,33 @@ function Main() {
         fetchApi()
     }, [])  // array vacio: el useEffect no tiene dependencias
 
+    
+    const formatingDate = (date) => {
+        const string = DateTime.toString(date)
+        const formated = DateTime.fromISO(string, {
+            zone: 'system',
+        })
+        console.log(formated)
+        return formated
+    }
+
+
     return(
         <>
             <h1>BUSQUEDA: "{search}"</h1>
             <h3>RESULTADOS TOTALES: {totalResult}</h3>
             <div className="conteiner-news">
                 { !toDos ? 'Cargando...' :
-                    toDos.articles.map( (toDo, index) => {
+                    toDos.articles.map( (news, index) => {
+
                         return(
                             <div className="box-news">
-                                <img src={toDo.urlToImage} className="img-news" />
-                                <p key={index}> <b>TITULO:</b> {toDo.title} </p> 
+                                <a href={news.url}>
+                                    <img src={news.urlToImage} className="img-news"  />
+                                    <p key={index}> <b>{news.title}</b>  </p> 
+                                    <small>{ news.publishedAt }</small>
+                                </a>
+                                
                             </div>
                         )
                     })
