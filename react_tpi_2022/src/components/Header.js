e6350f1283d458ddfc298c0fc3c9121854647115
louisrubin <miguel10.rbn@gmail.com>
2022-07-13 14:57:24 -0300

    update Header --> Header.js

--- react_tpi_2022/src/components/Header.js
+++ react_tpi_2022/src/components/Header.js
@@ -6,21 +6,29 @@ import './Header-css.css'
 function Header() {
     return(
         <>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Cormorant+SC:wght@700&family=EB+Garamond:ital,wght@0,800;1,700;1,800&family=Oswald:wght@300;400;700&display=swap');
+        </style>
             <div id="main">
                 <h1>INFOR-NOTICIAS</h1>
-                <div>Resistencia - 12°C - Dolar Blue: $287</div>
+                <div id="sub-info">
+                    <p>Resistencia - 12°C - Dolar Blue: $297</p>
                 
-                <div id="assets">
-                    <a href='https://www.linkedin.com/in/luisrubin/' placeholder='Linkedin' target="_blank">
-                        <img src={lk}/>
-                    </a>
-                    <a href='https://github.com/louisrubin' placeholder='GitHub' target="_blank">
-                        <img src={gh} />
-                    </a>
-                    <a href='coming-soon' placeholder='YouTube Video' target="_blank">
-                        <img src={yt} />
-                    </a>
+                    <div id="assets">
+                        <a href='https://www.linkedin.com/in/luisrubin/' placeholder='Linkedin' target="_blank">
+                            <img src={lk}/>
+                        </a>
+                        <a href='https://github.com/louisrubin' placeholder='GitHub' target="_blank">
+                            <img src={gh} />
+                        </a>
+                        <a href='coming-soon' placeholder='YouTube Video' target="_blank">
+                            <img src={yt} />
+                        </a>
+                    </div>
+                
                 </div>
+                
+                
             </div>
         </>
     )
