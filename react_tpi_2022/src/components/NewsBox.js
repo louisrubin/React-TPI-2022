8d7e180db081db8157575d3a7f444d6035232091
louisrubin <miguel10.rbn@gmail.com>
2022-07-17 12:35:23 -0300

    deleted align-items: center

--- react_tpi_2022/src/components/NewsBox.js
+++ react_tpi_2022/src/components/NewsBox.js
@@ -5,10 +5,15 @@ function NewsBox(props) {
     const { news, index, publishedAt } = props
     return(
         <div className="box-news" key={index}>
+            
             <a href={news.url} target="_blank" rel="noopener noreferrer">
                 <img className="img-news" src={news.urlToImage}  />
-                <small>Publicado: { publishedAt } - </small>
-                <small style={ {textDecoration: 'underline'} }>{ news.source.name }</small>
+            </a>
+
+            <small>Publicado: { publishedAt } - </small>
+            <small style={ {textDecoration: 'underline'} }>{ news.source.name }</small>
+
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
                 <p> <b>{news.title}</b>  </p> 
             </a>                                
         </div>
