6ee272ca5f03d3bfce996f6d4ffbe7a711e46db2
louisrubin <miguel10.rbn@gmail.com>
2022-07-18 12:34:51 -0300

    creating the SearchForm component --> News.js

--- react_tpi_2022/src/components/News.js
+++ react_tpi_2022/src/components/News.js
@@ -1,30 +1,58 @@
 import { useEffect, useState } from "react";
 import { DateTime } from 'luxon'
 import NewsBox from "./NewsBox";
+import './News.css'
 
 // CLASE 06/06
 
+function Form() {
+    // COMPONENT SEARCH BELOW HEADER
+    return(
+        <div id="form-search">
+            <form>
+                <input name="search" placeholder="Buscar" />
+
+                <label>Idioma</label>
+                <select name="language">
+                    <option>ES</option>
+                    <option>EN</option>
+                </select>
+
+                <label>Resultados</label>
+                <select name="pages">
+                    <option>4</option>
+                    <option>8</option>
+                    <option selected="yes">10</option>
+                    <option>12</option>
+                    <option>16</option>
+                </select>
+
+                <button type="submit">Buscar</button>
+            </form>
+        </div>
+    )
+}
+
 function Main() {
-    const search = 'argentina'
-    const pages = 10
-    const language = 'es'
+    const [search, setSearch] = useState('world')
+    const [pages, setPages] = useState(10)    // if (totalResults < 10 { pages = totalResults })
+    const [language, setLanguage] = useState('es')
+    const [allResp, setResp] = useState()
+    const [totalResult, setResults] = useState()
     
     const url = `https://newsapi.org/v2/everything?q=${search}&pageSize=${pages}&language=${language}&apiKey=3a8f8a50766947e8b6d4633919806d8a`
-    const [toDos, setToDos] = useState()
-    const [totalResult, setResult] = useState()
 
     const fetchApi = async () => {
         const response = await fetch(url)
         const responseJSON = await response.json()  // retorna una promise --> 'await'
         
-        setToDos(responseJSON)  // response
-        setResult(responseJSON.totalResults)    // total results
+        setResp(responseJSON)  // response
+        setResults(responseJSON.totalResults)    // total results
     }
 
-    // useEffect se ejecuta luego de montar el componente (componentDidMount)
-    useEffect( () =>{
-        fetchApi()
-    }, [])  // array vacio: el useEffect no tiene dependencias lo cual se ejecuta 1 sola vez al montar todo
+    // const handleChange(e) => {
+    //     e.onSubmit()    // ###############################
+    // }
 
     const transformDate = time => {
         // function to transform date ISO from News API to a local String Date
@@ -37,12 +65,18 @@ function Main() {
         //  console.log('Fecha: ', DateTime.fromISO(time).toLocaleString(DateTime.DATE_MED))    // 8 jul 2022
     }
 
+    
+    useEffect( () =>{
+        fetchApi()
+    }, [search, pages, language])  
+
     return(
         <>
+            <Form />
             <div className="conteiner-news">
-                { !toDos ? 'Cargando...' :
+                { !allResp ? 'Cargando...' :
 
-                    toDos.articles.map( (news, index ) => {
+                    allResp.articles.map( (news, index ) => {
                         const publishedAt = transformDate(news.publishedAt)
                         const obj = { news, index, publishedAt} // un objeto como param
                         return(
