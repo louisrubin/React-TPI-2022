badaddac64282347af3917561b4bb777ee1540b6
louisrubin <miguel10.rbn@gmail.com>
2022-07-19 11:13:31 -0300

    onSubmit 'search' --> News.js

--- react_tpi_2022/src/components/News.js
+++ react_tpi_2022/src/components/News.js
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import NewsBox from "./NewsBox";
 import './News.css'
 import Function from "./Functions";
+import notFoundicon from '../assets/file-delete.png'
 
 // CLASE 06/06
 
-function Form() {
+function Form(props) {
     // COMPONENT SEARCH BELOW HEADER
     return(
         <div id="form-search">
-            <form>
-                <input name="search" placeholder="Buscar" />
+            <form onSubmit={props.onSub}>
+                <input type="text" name="search" placeholder="Buscar" onChange={props.onChange} />
 
                 <label>Idioma</label>
                 <select name="language">
@@ -34,7 +35,7 @@ function Form() {
 }
 
 function Main() {
-    const [search, setSearch] = useState('world')
+    const [search, setSearch] = useState('chaco')
     const [pages, setPages] = useState(10)    // if (totalResults < 10 { pages = totalResults })
     const [language, setLanguage] = useState('es')
     const [allResp, setResp] = useState()
@@ -42,23 +43,30 @@ function Main() {
     
     const url = `https://newsapi.org/v2/everything?q=${search}&pageSize=${pages}&language=${language}&apiKey=3a8f8a50766947e8b6d4633919806d8a`
 
-    // const handleChange(e) => {
-    //     e.onSubmit()    // ###############################
-    // }
+    const onChange = (e) => {
+        setSearch( e.target.value )
+    }
+    const onSub = (e) => {
+        e.preventDefault()
+        Function.fetchApi(setResp, url)     // hace el llamado a la api
+    }
     
     useEffect( () =>{
         Function.fetchApi(setResp, url)     // import FetchApi
-    }, [search, pages, language])  
+    }, [])  
 
     return(
         <>
-            <Form />
+            <Form onSub={onSub} onChange={onChange} />
             <div className="conteiner-news">
                 { !allResp ? 'Cargando...' :
 
                     allResp.articles.map( (news, index ) => {
                         const publishedAt = Function.transformDate(news.publishedAt)    // import FetchApi
                         const obj = { news, index, publishedAt} // un objeto como param
+                        if (news.urlToImage === null){
+                            news.urlToImage = notFoundicon
+                        }
                         return(
                             NewsBox(obj)
                         )
