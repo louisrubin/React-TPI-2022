b894c47546ca24a003239bba2f518d6337d7c4a9
louisrubin <miguel10.rbn@gmail.com>
2022-07-25 20:37:39 -0300

    created LoadingSpinner Component

---
+++ react_tpi_2022/src/components/Spinner.js
@@ -0,0 +1,13 @@
+import './Spinner.css'
+
+function LoadingSpinner() {
+    // LOADING SPINNER COMPONENT
+    return(
+        <div className='spinner-container'>
+            <div className='loading-spinner'> </div>
+        </div>
+    )
+}
+
+export default LoadingSpinner;
+
