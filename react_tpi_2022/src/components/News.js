5319a55d4e05bb4833d54c1c8eac15e42a2be986
louisrubin <miguel10.rbn@gmail.com>
2022-08-03 19:12:04 -0300

    added 'pagination' Hook and modified API's URL --> News.js

--- react_tpi_2022/src/components/News.js
+++ react_tpi_2022/src/components/News.js
@@ -8,6 +8,7 @@ import FormSearch from './FormSearch';
 import TotalResults from './TotalResults'
 import LoadingSpinner from './Spinner';
 import NotFound_Results from './NotFound_Results';
+import Pagination from './Pagination';
 
 
 function NewsConteiner(props) {
@@ -52,17 +53,18 @@ function Main() {
     Functions.setTitleFunction('INFOR-NEWS')    // set title dynamically
     const [search, setSearch] = useState('argentina')
     const [newsPerPages, setNewsPerPages] = useState(8)
+    const [pagination, setPagination ] = useState(1)
     const [language, setLanguage] = useState('es')
     const [allResp, setAllResp] = useState()
-    
-    const urlNews = `https://newsapi.org/v2/everything?q=${search}&pageSize=${newsPerPages}&language=${language}&apiKey=3a8f8a50766947e8b6d4633919806d8a`
+    //                                                              searchIn=title (The fields to restrict your 'q' search to.)
+    const urlNews = `https://newsapi.org/v2/everything?q=${search}&searchIn=title&pageSize=${newsPerPages}&page=${pagination}&language=${language}&apiKey=3a8f8a50766947e8b6d4633919806d8a`
 
 
     // component did mount 
     useEffect( () =>{
         Functions.fetchNews(setAllResp, urlNews, newsPerPages, setNewsPerPages)     // import SET ALL RESP & PAGES
         Functions.formListener( {setSearch, setLanguage, setNewsPerPages} )
-    }, [search, language, newsPerPages])    // useEffect's dependencies
+    }, [search, language, newsPerPages, pagination])    // useEffect's dependencies
     
     
 
@@ -82,6 +84,8 @@ function Main() {
                         <div className="conteiner-news">
                             <NewsConteiner allResp={allResp} />
                         </div>
+
+                        <Pagination pagination={pagination} setPagination={setPagination} />
                     </>
                 }
         </>
