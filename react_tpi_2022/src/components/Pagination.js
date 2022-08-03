60f53f8b82ab49cd64e94fc10426ead62cecdf19
louisrubin <miguel10.rbn@gmail.com>
2022-08-03 19:08:18 -0300

    added styles --> Pagination.js

--- react_tpi_2022/src/components/Pagination.js
+++ react_tpi_2022/src/components/Pagination.js
@@ -2,29 +2,36 @@ import './Pagination.css'
 import prevIcon from '../assets/arrow-prev.png'
 import nextIcon from '../assets/arrow-next.png'
 
-function Pagination(state) {
+function Pagination( {pagination, setPagination} ) {
 
     const prevPage = () => {
         // previous page function
-        console.log('clic anterior')
+
+        if(pagination > 1){
+            // if 'pagination' is less than 1 it doesn't nothing
+            setPagination(pagination -= 1)
+        }
     }
 
     const nextPage = () => {
         // next page function
-        console.log('clic siguiente')
+        setPagination(pagination += 1)
     }
 
     return(
         <div className="pagination-container">
-            <nav onClick={prevPage}>
-                <img src={prevIcon} />
-            </nav>
-            <nav>
-                1
-            </nav>
-            <nav onClick={nextPage}>
-                <img src={nextIcon} />
-            </nav>
+            <div className='pagination-ELEMENTS'>
+                <nav onClick={prevPage}>
+                    <img src={prevIcon} />
+                </nav>
+                <nav id='number-Page'>
+                    {pagination}
+                </nav>
+                <nav onClick={nextPage}>
+                    <img src={nextIcon} />
+                </nav>
+
+            </div>
         </div>
     )
 }
