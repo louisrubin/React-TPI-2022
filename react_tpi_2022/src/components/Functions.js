9ca1df72ab56588f9517c49642c7c37c8fd05757
louisrubin <miguel10.rbn@gmail.com>
2022-08-09 23:07:41 -0300

    Update the 'buscarFormListener' function --> Functions.js

--- react_tpi_2022/src/components/Functions.js
+++ react_tpi_2022/src/components/Functions.js
@@ -62,45 +62,83 @@ const Functions = {
 
         // EVENT LISTENER FOR ENABLE 'FORM BUTTON'
         input.addEventListener('blur', () => {
-                // if input value is greater than
+            // if input value is greater than
 
-                if (input.value.length < 3 ){
-                    // if value is less than 'value'
-                    input.classList.add('lessThanThree')    // borde rojo al input
-                    textAlert.classList.remove("hiden")     // elimina la clase CSS '.hiden'
-                    submitBtn.disabled = true               // desactiva el button
-                
-                } else{
-                    // else value is greater than 'value'
-                    input.classList.remove('lessThanThree')
-                    textAlert.classList.add("hiden")
-                    submitBtn.disabled = false
-                }
-            }  
-        ) 
+            if (input.value.length < 3 ){
+                // if value is less than 'value'
+                input.classList.add('lessThanThree')    // borde rojo al input
+                textAlert.classList.remove("hiden")     // elimina la clase CSS '.hiden'
+                submitBtn.disabled = true               // desactiva el button
+            
+            } else{
+                // else value is greater than 'value'
+                input.classList.remove('lessThanThree')
+                textAlert.classList.add("hiden")
+                submitBtn.disabled = false
+            }
+        }) 
 
         // FORM EVENT LISTENER FOR 'SUBMIT EVENT'
         form.addEventListener('submit', e => {      // form listener on Submit event
-                e.preventDefault()
+            e.preventDefault()
 
-                const data = Object.fromEntries(new FormData(e.target) )
-                setSearch(data.search)
-                setLanguage( data.language.slice(0, 2) )
-                setNewsPerPages(data.pages)
-                setPagination(1)    // SET PAGINATION TO 1
-                return navigateMain(`/buscar/${data.search}/${1}`)      // Navigate MAIN component
-            }
-        )
+            const data = Object.fromEntries(new FormData(e.target) )
+            setSearch(data.search)
+            setLanguage( data.language.slice(0, 2) )
+            setNewsPerPages(data.pages)
+            setPagination(1)    // SET PAGINATION TO 1
+            return navigateMain(`/buscar/${data.search}/${1}`)      // Navigate MAIN component
+        })
     },
 
-    buscarFormListener: ( navigate ) =>{
-        // received 'navigate' hook
-        const buscarForm = document.getElementById('buscar-form')
+    buscarFormListener: ( idForm, idInput, submitBtn, textAlertID , iconParam=null, navigate = null ) =>{
 
-        buscarForm.addEventListener('submit', e => {
+        const form = document.getElementById(idForm)
+        const input = document.getElementById(idInput)
+        const submitButton = document.getElementById(submitBtn)
+        const alertMessage = document.getElementById(textAlertID)
+
+        let icon = undefined
+        if(iconParam){
+            icon = document.getElementById(iconParam)
+        }
+
+        alertMessage.classList.add("hiden")    // agrega la clase CSS 'hiden'
+        submitButton.disabled = true
+
+        // EVENT LISTENER FOR ENABLE 'FORM BUTTON'
+        input.addEventListener('blur', () => {
+            // if input value is greater than
+
+            if (input.value.length < 3 ){
+                // if value is less than 'value'
+                input.classList.add('lessThanThree')       // borde rojo al input
+                alertMessage.classList.remove("hiden")     // elimina la clase CSS '.hiden'
+                submitButton.disabled = true               // desactiva el button
+
+                submitButton.classList.remove("button-styles")
+                icon.classList.remove("icon-styles")
+            
+            } else{
+                // else value is greater than 'value'
+                input.classList.remove('lessThanThree')
+                alertMessage.classList.add("hiden")
+                submitButton.disabled = false
+
+                submitButton.classList.add("button-styles")
+                icon.classList.add("icon-styles")
+            }
+        }) 
+
+
+        form.addEventListener('submit', e => {
             e.preventDefault()
             const data = Object.fromEntries( new FormData(e.target) )
-            return navigate(`/buscar/${data.buscar}/${1}`)   // return the navigate hook received from params with the value from the from
+
+            if (navigate){
+                //  if received 'navigate' hook
+                return navigate(`/buscar/${data.buscar}/${1}`)   // return the navigate hook received from params with the value from the from
+            }
         })
 
     },
